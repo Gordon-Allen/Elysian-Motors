e09154cbd7c1723376ba8f9f9222c4cc3bfff372
Gordon-Allen <gordonmichaelallen@gmail.com>
2019-12-05 16:54:27 -0500

    Updated DB migrations with new changes

--- Migrations/20191205205522_ModelsValidationsUpdateMigration.Designer.cs
+++ Migrations/20191205205522_ModelsValidationsUpdateMigration.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using ElysianMotors.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ElysianMotors.Migrations
 {
     [DbContext(typeof(MyContext))]
-    partial class MyContextModelSnapshot : ModelSnapshot
+    [Migration("20191205205522_ModelsValidationsUpdateMigration")]
+    partial class ModelsValidationsUpdateMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
