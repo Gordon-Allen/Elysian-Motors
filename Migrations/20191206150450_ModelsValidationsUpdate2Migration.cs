240d58cc294364682894eb45968e44a2270441cd
Gordon-Allen <gordonmichaelallen@gmail.com>
2019-12-06 10:08:07 -0500

    Updated Models to support purchase logic

---
+++ Migrations/20191206150450_ModelsValidationsUpdate2Migration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ElysianMotors.Migrations
+{
+    public partial class ModelsValidationsUpdate2Migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PurchasePrice",
+                table: "Orders",
+                nullable: true,
+                oldClrType: typeof(double));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "PurchasePrice",
+                table: "Orders",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
--- Models/MyModel.cs
+++ Models/MyModel.cs
@@ -59,7 +59,7 @@ namespace ElysianMotors.Models
         public int OrderId { get; set; }
         public int VehicleID { get; set; }
         public string VehicleName {get; set;}
-        public double PurchasePrice {get; set;}
+        public string PurchasePrice {get; set;}
 
         [Required]
         [MinLength(2, ErrorMessage = "Your 'First-Name' has to be at least (2) characters")]
