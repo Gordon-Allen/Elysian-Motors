ee34988952a66ea6478358b0cc8aa5d588901b8b
Gordon-Allen <gordonmichaelallen@gmail.com>
2020-03-09 13:46:37 -0400

    Added functional routing to seperated Controllers

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,39 +9,11 @@ using ElysianMotors.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
-using Google.Apis.Auth.OAuth2;
-using Google.Apis.Services;
-using Google.Apis.Sheets.v4;
-using Google.Apis.Sheets.v4.Data;
 
 namespace ElysianMotors.Controllers
 {
     public class HomeController : Controller
     {
-        // static readonly string[] Scopes = { SheetsService.Scope.Spreadsheets };
-        // static readonly string ApplicationName = "<<ENTER NAME OF PROJECT YOU SPECIFIED IN GOOGLE DEVELOPER CONSOLE>>";
-        // static readonly string sheet = "<<ENTER NAME OF SPECFIC SHEET WITHIN YOUR GOOGLE SHEETS SPREADSHEET YOU WISH TO SEND INFO TO (DEFAULT IS 'Sheet 1')>>";
-        // static readonly string SpreadsheetId = "<<ENTER GOOGLE SHEETS SPREADSHEET ID, FROUND IN GOOGLE SHEETS URL>>";
-        // static SheetsService service;
-
-        // static void GoogleSheets_Init(){
-
-        // GoogleCredential credential;
-
-        // //Reading Credentials File...
-        // using (var stream = new FileStream("<<ENTER NAME OF GOOGLE SHEETS CREDENTIALS JSON FILE>>", FileMode.Open, FileAccess.Read))
-        // {
-        //     credential = GoogleCredential.FromStream(stream)
-        //         .CreateScoped(Scopes);
-        // }
-
-        // // Creating Google Sheets API service...
-        // service = new SheetsService(new BaseClientService.Initializer()
-        // {
-        //     HttpClientInitializer = credential,
-        //     ApplicationName = ApplicationName,
-        // });
-        // }
 
         private MyContext dbContext;
 
@@ -61,130 +33,5 @@ namespace ElysianMotors.Controllers
         {
             return View();
         }
-
-        [HttpGet("AddVehicle")]
-        public IActionResult AddVehicle()
-        {
-            return View();
-        }
-
-        [HttpGet("SellerList")]
-        public IActionResult SellerList()
-        {
-            List<Vehicle> allVehicles = dbContext.Vehicles
-            .OrderBy(v => v.Year)
-            .ToList();
-            ViewBag.AllVehicles = allVehicles;
-            return View();
-
-        }
-
-        [Route("btAddVehicle")]
-        [HttpPost]
-        public IActionResult btAddVehicle(Vehicle newVehicle)
-        {
-            Console.WriteLine(newVehicle.VehicleID);
-            Console.WriteLine(newVehicle.Year);
-            Console.WriteLine(newVehicle.Make);
-            Console.WriteLine(newVehicle.Model);
-            Console.WriteLine(newVehicle.Color);
-            Console.WriteLine(newVehicle.Mileage);
-            Console.WriteLine(newVehicle.VehicleType);
-            Console.WriteLine(newVehicle.EngineType);
-            Console.WriteLine(newVehicle.NumberOfSeats);
-            Console.WriteLine(newVehicle.ImageUrl);
-            Console.WriteLine(newVehicle.Price);
-
-            if (ModelState.IsValid){
-                dbContext.Add(newVehicle);
-                dbContext.SaveChanges();
-                return RedirectToAction("SellerList");
-            }
-            else
-            {
-                Console.WriteLine("<--------- Error Adding VEHICLE to DB -----------> ");
-                return View("AddVehicle");
-            }        
-        }
-
-        [Route("detail/{id}")]
-        [HttpGet]
-        public IActionResult VehicleDetail(int id)
-        {
-            Vehicle v = dbContext.Vehicles
-            .FirstOrDefault(pro => pro.VehicleID == id);
-
-            TempData["pVehicleID"] = id;
-            ViewBag.VehicleDetail = v;
-            return View("VehicleDetail");
-        }
-
-        [Route("detail/purchase/{id}")]
-        [HttpGet]
-        public IActionResult PurchaseVehicle(int id)
-        {
-            Vehicle purchaseV = dbContext.Vehicles
-            .FirstOrDefault(pro => pro.VehicleID == id);
-            ViewBag.VehicleDetail = purchaseV;
-            return View("PurchaseVehicle");
-        }
-
-        [Route("btCreateOrder")]
-        [HttpPost]
-        public IActionResult btCreateOrder(int? id, Order newOrder)
-        {
-            Vehicle confirmpurchaseV = dbContext.Vehicles
-            .FirstOrDefault(pro => pro.VehicleID == id);
-            newOrder.VehicleID = confirmpurchaseV.VehicleID;
-            newOrder.VehicleName = confirmpurchaseV.Year + " " + confirmpurchaseV.Make + " " + confirmpurchaseV.Model;
-            newOrder.PurchasePrice = confirmpurchaseV.Price;
-
-            Console.WriteLine(newOrder.OrderId);
-            Console.WriteLine(newOrder.VehicleID);
-            Console.WriteLine(newOrder.VehicleName);
-            Console.WriteLine(newOrder.CustomerFirstName);
-            Console.WriteLine(newOrder.CustomerLastName);
-            Console.WriteLine(newOrder.CustomerEmail);
-            Console.WriteLine(newOrder.PurchasePrice);
-            Console.WriteLine(newOrder.OrderDate);
-
-            if (ModelState.IsValid){
-                dbContext.Add(newOrder);
-                Vehicle removePurchaseV = dbContext.Vehicles.FirstOrDefault(web => web.VehicleID == newOrder.VehicleID);
-                dbContext.Remove(removePurchaseV);
-                dbContext.SaveChanges();
-
-                // GoogleSheets_Init();
-                // var range = $"{sheet}!A:H";
-                // var valueRange = new ValueRange();
-
-                // // Data for Order...
-                // var oblist = new List<object>() { newOrder.OrderId, newOrder.VehicleID, newOrder.VehicleName, newOrder.CustomerFirstName, newOrder.CustomerLastName, newOrder.CustomerEmail, newOrder.PurchasePrice, newOrder.OrderDate};
-                // valueRange.Values = new List<IList<object>> { oblist };
-
-                // // Append the above record To 'Elysian Motors' G-Sheet..
-                // var appendRequest = service.Spreadsheets.Values.Append(valueRange, SpreadsheetId, range);
-                // appendRequest.ValueInputOption = SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum.USERENTERED;
-                // var appendReponse = appendRequest.Execute();
-                
-                return RedirectToAction("PurchaseConfirmation", new {id = newOrder.OrderId});
-            }
-            else
-            {
-                Console.WriteLine("<--------- Error Adding ORDER to DB -----------> ");
-                return View("PurchaseVehicle");
-            }        
-        }
-
-        [Route("purchaseconfirmation/{id}")]
-        [HttpGet]
-        public IActionResult PurchaseConfirmation(int id)
-        {
-            Order confirmOrder = dbContext.Orders
-            .FirstOrDefault(pro => pro.OrderId == id);
-            ViewBag.OrderDetail = confirmOrder;
-            return View("PurchaseConfirmation");
-        }
-
     }
 }
---
+++ Controllers/VehicleController.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ElysianMotors.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ElysianMotors.Controllers
+{
+    public class VehicleController : Controller
+    {
+
+        private MyContext dbContext;
+
+        public VehicleController(MyContext context)
+        {
+            dbContext = context;
+        }
+
+        [HttpGet("Vehicle/AddVehicle")]
+        public IActionResult AddVehicle()
+        {
+            return View();
+        }
+
+        [HttpGet("Vehicle/SellerList")]
+        public IActionResult SellerList()
+        {
+            List<Vehicle> allVehicles = dbContext.Vehicles
+            .OrderBy(v => v.Year)
+            .ToList();
+            ViewBag.AllVehicles = allVehicles;
+            return View();
+
+        }
+
+        [Route("btAddVehicle")]
+        [HttpPost]
+        public IActionResult btAddVehicle(Vehicle newVehicle)
+        {
+            Console.WriteLine(newVehicle.VehicleID);
+            Console.WriteLine(newVehicle.Year);
+            Console.WriteLine(newVehicle.Make);
+            Console.WriteLine(newVehicle.Model);
+            Console.WriteLine(newVehicle.Color);
+            Console.WriteLine(newVehicle.Mileage);
+            Console.WriteLine(newVehicle.VehicleType);
+            Console.WriteLine(newVehicle.EngineType);
+            Console.WriteLine(newVehicle.NumberOfSeats);
+            Console.WriteLine(newVehicle.ImageUrl);
+            Console.WriteLine(newVehicle.Price);
+
+            if (ModelState.IsValid){
+                dbContext.Add(newVehicle);
+                dbContext.SaveChanges();
+                return RedirectToAction("SellerList");
+            }
+            else
+            {
+                Console.WriteLine("<--------- Error Adding VEHICLE to DB -----------> ");
+                return View("AddVehicle");
+            }        
+        }
+
+        [Route("Vehicle/detail/{id}")]
+        [HttpGet]
+        public IActionResult VehicleDetail(int id)
+        {
+            Vehicle v = dbContext.Vehicles
+            .FirstOrDefault(pro => pro.VehicleID == id);
+
+            TempData["pVehicleID"] = id;
+            ViewBag.VehicleDetail = v;
+            return View("VehicleDetail");
+        }
+
+    }
+}
