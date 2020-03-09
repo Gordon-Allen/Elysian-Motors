12111ecd95bf45633252840147f0df41e5bc8690
Gordon-Allen <gordonmichaelallen@gmail.com>
2020-03-09 15:16:56 -0400

    Altered Vehicle 'ImageUrl' Display name

---
+++ Migrations/20200309191039_UpdatedVehicleImageURLDisplay.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ElysianMotors.Migrations
+{
+    public partial class UpdatedVehicleImageURLDisplay : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
